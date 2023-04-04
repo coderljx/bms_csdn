c4450b95cd2ba031972d1ac866e404773a4a2c7f
Brother <948917676@qq.com>
2023-04-04 20:38:34 +0800

    status

---
+++ src/components/content/V/content.vue
@@ -0,0 +1,15 @@
+<template>
+  <headers></headers>
+
+  <div class="">
+
+  </div>
+</template>
+
+<script setup>
+import headers from "@/components/Header/header.vue"
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/wechat/wechat.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "wechat"
+}
+</script>
+
+<style scoped>
+
+</style>
